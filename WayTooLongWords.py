d8d40474b94f7f3d4858fe669557c71c0e61be25
Afueth Thomas <97304915+Afueth@users.noreply.github.com>
2026-07-09 11:44:53 +0530

    Commit

--- WayTooLongWords.py
+++ WayTooLongWords.py
@@ -7,3 +7,15 @@ for word in words:
        print(word[0] + str(len(word) - 2) + word[-1])
     else: 
          print(word)
+         
+         
+         
+# Efficent Method      
+# n = int(input())
+
+# for _ in range(n):
+#     word = input()
+#     if len(word) > 10:
+#         print(word[0] + str(len(word) - 2) + word[-1])
+#     else:
+#         print(word)
